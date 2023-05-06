69c092e4157c592df8daa9b7dc57fa2679f29149
Pierre <pierrefraisse90@gmail.com>
2023-05-06 19:40:16 +0200

    logic

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import './Login.scss'
 import {AiOutlineEye, AiOutlineEyeInvisible} from 'react-icons/ai'
+import { useState } from 'react'
 
 const Login = () => {
+    const [showPassword, setShowPassword] = useState(false);
+    const togglePassword = () => {
+        setShowPassword(!showPassword)
+    };
+
   return (
     <section className='login --center-all --100vh'>
         <div className="--card --bg-light --p">
@@ -10,10 +16,16 @@ const Login = () => {
             <div className="--form-control">
                 <input type="text" placeholder='Username' className='--width-100' />
                 <div className="password">
-                    <input type="password" placeholder='Password' className='--width-100' />
-                    <div className="icon">
-                        <AiOutlineEye />
-                        {/* <AiOutlineEyeInvisible /> */}
+                    <input 
+                    type={showPassword ? "text" : "password"} 
+                    placeholder='Password' 
+                    className='--width-100' 
+                    />
+                    <div className="icon" onClick={togglePassword}>
+                        {showPassword ? 
+                            <AiOutlineEyeInvisible /> :
+                            <AiOutlineEye />
+                        }
                     </div>
                 </div>
                 <button className='--btn --btn-danger --btn-block'>Login</button>
